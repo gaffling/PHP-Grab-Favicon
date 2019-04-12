f138a1e68d4af368e5808487ebd8d77e2a58ffaf
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2019-04-12 09:16:06 +0200

    ✓ Improvement
    API is only used if local favicon file not already exists

--- quick-and-dirty-version.php
+++ quick-and-dirty-version.php
@@ -3,19 +3,20 @@
 /* Get Favicon of given URL, save it an return the PNG Favicon Image Path */
 
 $url = 'https://github.com/';
-
 echo '<img src="'.save_favicon($url).'">';
 
 function save_favicon($url, $path='./') {
-  $saveFileName = parse_url($url, PHP_URL_HOST);
-  $fp = fopen ($path.$saveFileName.'.png', 'w+');
-  $ch = curl_init('http://www.google.com/s2/favicons?domain='.$url);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-  curl_setopt($ch, CURLOPT_FILE, $fp); /* Save the returned Data to File */
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_exec($ch);
-  curl_close($ch);
-  fclose($fp);
-
-  return $path.$saveFileName.'.png';
+  $url = parse_url($url, PHP_URL_HOST);
+  $file = $path.$url.'.png';
+  if ( !file_exists()) {
+  $fp = fopen ($file, 'w+');
+    $ch = curl_init('http://www.google.com/s2/favicons?domain='.$url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+    curl_setopt($ch, CURLOPT_FILE, $fp); /* Save the returned Data to File */
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+  }
+  return $file;
 }
